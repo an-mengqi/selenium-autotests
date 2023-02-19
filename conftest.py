fa76bf354d1cc0966f7ea5e85763dcfbea6be3b1
Anastasiia Monakhova <a.klopova@dssl.ru>
2023-02-19 17:26:59 +0100

    logging added

--- conftest.py
+++ conftest.py
@@ -1,25 +1,33 @@
+import datetime
 import os
 import pytest
+import logging
 
 from selenium import webdriver
 
 
 def pytest_addoption(parser):
-    parser.addoption(
-        "--browser", default = "chrome", help = "choose browser: chrome, firefox, yandex"
-    )
-    parser.addoption(
-        "--driver_folder", default = os.path.expanduser("~/drivers")
-    )
-    parser.addoption(
-        "--url", action="store", default="http://192.168.0.18:8081"
-    )
+    parser.addoption("--browser", default="chrome", help="choose browser: chrome, firefox, yandex")
+    parser.addoption("--driver_folder", default=os.path.expanduser("~/otus/drivers"))
+    parser.addoption("--url", action="store", default="http://192.168.0.15:8081")
+    parser.addoption("--log_level", action="store", default="DEBUG")
+
+
 @pytest.fixture
 def browser(request):
     _browser = request.config.getoption("--browser")
     driver_folder = request.config.getoption("--driver_folder")
     driver = None
     url = request.config.getoption("--url")
+    log_level = request.config.getoption("--log_level")
+
+    logger = logging.getLogger(request.node.name)
+    file_handler = logging.FileHandler(f"logs/{request.node.name}.log")
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(level=log_level)
+
+    logger.info("===> Test {} started at {}".format(request.node.name, datetime.datetime.now()))
 
     if _browser == "firefox" or _browser == "ff":
         driver = webdriver.Firefox(firefox_binary="/usr/bin/firefox", executable_path=f"{driver_folder}{os.sep}geckodriver")
@@ -28,10 +36,17 @@ def browser(request):
     elif _browser == "yandex":
         driver = webdriver.Chrome(executable_path=f"{driver_folder}{os.sep}yandexdriver")
 
+    driver.log_level = log_level
+    driver.logger = logger
+    driver.test_name = request.node.name
+
+    logger.info("Browser:{}".format(browser, driver.capabilities))
+
     driver.maximize_window()
 
     driver.get(url)
     driver.url = url
 
     yield driver
+    logger.info("===> Test {} finished at {}".format(request.node.name, datetime.datetime.now()))
     driver.close()
--- page_objects/AdminPage.py
+++ page_objects/AdminPage.py
@@ -13,6 +13,7 @@ class AdminPage(BasePage):
     DASHBOARD_PAGE_TITLE = (By.XPATH, "//*[@id='content']/div[1]/div/h1")
 
     def login(self, user_name, user_password):
+        self.logger.info("Login as {}".format(user_name))
         self.input_value(self.USERNAME_INPUT_FIELD, user_name)
         self.input_value(self.PASSWORD_INPUT_FIELD, user_password)
         self.click_element(self.LOGIN_BUTTON)
